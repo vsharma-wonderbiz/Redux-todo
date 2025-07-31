1d283c13e071b66d5474bbe1ef7dd633d27e921f
unknown <you@example.com>
2025-07-31 12:26:33 +0530

    adding the complete project

--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -1,37 +1,101 @@
-import type { RootState } from "../App/Store";
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { removeTodo } from "../Features/TodoSlice";
+// import type { RootState } from "../App/Store";
+// import React from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { removeTodo } from "../Features/TodoSlice";
 
 
-const Todos=()=>{
+// const Todos=()=>{
 
-    const todos=useSelector( (state:RootState)=>state.todo.todos)
-    const dispatch=useDispatch();
+//     const todos=useSelector( (state:RootState)=>state.todo.todos)
+//     const dispatch=useDispatch();
 
-    return(
-        <>
+//     return(
+//         <>
         
-            {
-            todos.map((e)=>{
-                return(
+//             {
+//             todos.map((e)=>{
+//                 return(
                     
-                     <li
-                  key={e.id}
-                >
-                   {e.text}
+//                      <li
+//                   key={e.id}
+//                 >
+//                    {e.text}
 
-                   <button onClick={ () =>dispatch(removeTodo(e.id))}>delete</button>
-                </li>
+//                    <button onClick={ () =>dispatch(removeTodo(e.id))}>delete</button>
+//                 </li>
                 
-                )
+//                 )
                
-            })
-          }
+//             })
+//           }
           
           
-        </>
-    )
-}
+//         </>
+//     )
+// }
+
+// export default Todos;
+
+
+
+import type { RootState } from "../App/Store";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeTodo, editTodo } from "../Features/TodoSlice";
+
+const Todos = () => {
+  const todos = useSelector((state: RootState) => state.todo.todos);
+  const dispatch = useDispatch();
+
+  // Track which todo is being edited and its new text
+  const [editId, setEditId] = useState<number | null>(null);
+  const [editText, setEditText] = useState<string>("");
+
+  return (
+    <>
+      {todos.map((e) => {
+        return (
+          <li key={e.id}>
+            {editId === e.id ? (
+              <>
+                {/* If editing, show input */}
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={(ev) => setEditText(ev.target.value)}
+                />
+                <button
+                  onClick={() => {
+                    dispatch(editTodo({ id: e.id, text: editText }));
+                    setEditId(null); // exit edit mode
+                  }}
+                >
+                  Save
+                </button>
+                <button onClick={() => setEditId(null)}>Cancel</button>
+              </>
+            ) : (
+              <>
+                {/* Normal view */}
+                {e.text}
+                <button onClick={() => dispatch(removeTodo(e.id))}>
+                  Delete
+                </button>
+                <button
+                  onClick={() => {
+                    setEditId(e.id);
+                    setEditText(e.text);
+                  }}
+                >
+                  Edit
+                </button>
+              </>
+            )}
+          </li>
+        );
+      })}
+    </>
+  );
+};
 
 export default Todos;
--- src/Features/TodoSlice.tsx
+++ src/Features/TodoSlice.tsx
@@ -1,5 +1,4 @@
-import { createSlice,nanoid, type PayloadAction } from "@reduxjs/toolkit";
-
+import { createSlice, nanoid, type PayloadAction } from "@reduxjs/toolkit";
 
 interface Todo {
   id: number;
@@ -11,31 +10,36 @@ interface TodoState {
 }
 
 const initialState: TodoState = {
-  todos: [{id:1,text:"hello bantai"}]
+  todos: [{ id: 1, text: "hello bantai" }]
 };
 
+export const TodoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    AddTodo: (state, action: PayloadAction<string>) => {
+      const todo = {
+        id: Date.now(),
+        text: action.payload
+      };
+      state.todos.push(todo);
+    },
+
+    removeTodo: (state, action: PayloadAction<number>) => {
+      state.todos = state.todos.filter((e) => e.id !== action.payload);
+    },
 
 
-export  const TodoSlice=createSlice({
-       name:'todo',
-       initialState,
-       reducers:{
-        AddTodo:(state,action :PayloadAction<string>)=>{
-             const todo={
-                id:Date.now(),
-                text:action.payload
-             }
-             state.todos.push(todo);
-        },
-        
-            removeTodo:(state,action:PayloadAction<number>)=>{
-                state.todos=state.todos.filter((e)=>{
-               return  e.id!==action.payload
-                })
-            }
-       }
-})
+    
+    editTodo: (state, action: PayloadAction<{ id: number; text: string }>) => {
+      const todo = state.todos.find((e) => e.id === action.payload.id);
+      if (todo) {
+        todo.text = action.payload.text;
+      }
+    }
+  }
+});
 
-export const {AddTodo,removeTodo} =TodoSlice.actions
+export const { AddTodo, removeTodo, editTodo } = TodoSlice.actions;
 
-export default TodoSlice.reducer
+export default TodoSlice.reducer;
